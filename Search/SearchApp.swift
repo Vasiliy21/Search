3888e20bd582065893524224ab43c1df1726945b
Vasiliy Startsev <Vasiliystartsev@gmail.com>
2023-10-14 14:31:08 +0300

    Initial Commit

---
+++ Search/SearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchApp.swift
+//  Search
+//
+//  Created by Vasiliy on 14.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct SearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
